9cb75433c6b6868263147242e144226117a51f3b
dimi <dimitri4d@gmail.com>
2017-03-10 22:41:16 -0500

    added test data

--- openstreetmap.py
+++ openstreetmap.py
@@ -1,5 +1,25 @@
 import folium
+import pandas
 
-mymap = folium.Map
-mymap = folium.Map(location=[45.372,-121.697],zoom_start=12,tiles='Stamen Terrain')
-mymap.save('test.html')
+df = pandas.read_csv("data/Volcanoes-USA.txt")
+
+
+map_1 = folium.Map(location=[45.372,-121.697],zoom_start=4,tiles='Stamen Toner')
+
+def marker_color(elev):
+    if elev in range(0,1000):
+        col = 'green'
+    elif elev in range(1000,3000):
+        col = 'orange'
+    else:
+        col ='red'
+    return col
+
+
+
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
+
+    folium.Marker([lat, lon],popup = name, icon = folium.Icon(color = marker_color(elev))).add_to(map_1)
+
+
+map_1.save('test.html')
